e6042ad10dcd051db6396dd042ae5bdc3aed3f69
gejunling <33381926@qq.com>
2021-01-29 18:25:57 +0800

    首页调整

--- dashboard-vue/src/App.vue
+++ dashboard-vue/src/App.vue
@@ -4,6 +4,7 @@
       <el-header>
         <!-- 头部 导航 -->
         <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+          <img src="./assets/kotlin.png" style="float: left;height: 60px;width: 130px;">
           <el-menu-item index="1">处理中心</el-menu-item>
           <el-submenu index="2">
             <template #title>我的工作台</template>
@@ -18,56 +19,62 @@
             </el-submenu>
           </el-submenu>
           <el-menu-item index="3" disabled>消息中心</el-menu-item>
-          <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+          <el-menu-item index="4">订单管理</el-menu-item>
         </el-menu>
+
       </el-header>
+
       <el-container>
         <el-aside width="150px">
-            <el-col :span="24">
-              <el-menu
-                  default-active="2"
-                  class="el-menu-vertical-demo"
-                  @open="handleOpen"
-                  @close="handleClose">
-                <el-submenu index="1">
-                  <template #title>
-                    <i class="el-icon-location"></i>
-                    <span>导航一</span>
-                  </template>
-                  <el-menu-item-group>
-                    <template #title>分组一</template>
-                    <el-menu-item index="1-1">选项1</el-menu-item>
-                    <el-menu-item index="1-2">选项2</el-menu-item>
-                  </el-menu-item-group>
-                  <el-menu-item-group title="分组2">
-                    <el-menu-item index="1-3">选项3</el-menu-item>
-                  </el-menu-item-group>
-                  <el-submenu index="1-4">
-                    <template #title>选项4</template>
-                    <el-menu-item index="1-4-1">选项1</el-menu-item>
-                  </el-submenu>
+          <el-col :span="24">
+            <el-menu
+                default-active="2"
+                class="el-menu-vertical-demo"
+                @open="handleOpen"
+                @close="handleClose"
+                @select="handleSelect"
+            >
+              <el-submenu index="1">
+                <template #title>
+                  <i class="el-icon-location"></i>
+                  <span>导航一</span>
+                </template>
+                <el-menu-item-group>
+                  <template #title>分组一</template>
+                  <el-menu-item index="1-1">选项1</el-menu-item>
+                  <el-menu-item index="1-2">选项2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="分组2">
+                  <el-menu-item index="1-3">选项3</el-menu-item>
+                </el-menu-item-group>
+                <el-submenu index="1-4">
+                  <template #title>选项4</template>
+                  <el-menu-item index="1-4-1">选项1</el-menu-item>
                 </el-submenu>
-                <el-menu-item index="2">
-                  <i class="el-icon-menu"></i>
-                  <template #title>导航二</template>
-                </el-menu-item>
-                <el-menu-item index="3" disabled>
-                  <i class="el-icon-document"></i>
-                  <template #title>导航三</template>
-                </el-menu-item>
-                <el-menu-item index="4">
-                  <i class="el-icon-setting"></i>
-                  <template #title>导航四</template>
-                </el-menu-item>
-              </el-menu>
-            </el-col>
+              </el-submenu>
+              <el-menu-item index="2">
+                <i class="el-icon-menu"></i>
+                <template #title>导航二</template>
+              </el-menu-item>
+              <el-menu-item index="3" disabled>
+                <i class="el-icon-document"></i>
+                <template #title>导航三</template>
+              </el-menu-item>
+              <el-menu-item index="4"       @select="handleSelect">
+                <i class="el-icon-setting"></i>
+                <template #title>导航四</template>
+              </el-menu-item>
+            </el-menu>
+          </el-col>
         </el-aside>
 
 
-        <el-main>Main</el-main>
-
+        <el-main>
+          <router-view/>
+        </el-main>
 
       </el-container>
+
       <el-footer>footer</el-footer>
     </el-container>
   </div>
@@ -82,12 +89,11 @@ export default {
       activeIndex2: '1'
     };
   },
-  components: {
-
-  },
+  components: {},
   methods: {
     handleSelect(key, keyPath) {
       console.log(key, keyPath);
+      this.$router.push(key)
     },
     handleOpen(key, keyPath) {
       console.log(key, keyPath);
@@ -137,7 +143,8 @@ body > .el-container {
 .el-container:nth-child(7) .el-aside {
   line-height: 320px;
 }
-.el-menu-vertical-demo{
+
+.el-menu-vertical-demo {
   min-width: 150px;
 }
 
